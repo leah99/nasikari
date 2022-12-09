aade0a6f398b0a450aee46f0756df90bf2090704
Shi Min <shimin.loww@gmail.com>
2022-12-09 22:53:28 +0800

    feat(app): skeleton of project

---
+++ nasikari/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomePage.scss';
+
+function HomePage() {
+    return (
+        <div className="home">
+        </div>
+      );
+}
+
+export default HomePage;
